831445dcda7b3edb431359b959071192821b84cf
Avery Myers <averyjmyers@hotmail.com>
2023-10-03 17:40:39 -0700

    working queries for login and add user

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,25 +1,27 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
+const { Types } = require('mongoose');
 
 
 const resolvers = {
   Query: {
-    me: async (parent, args, context) => {
-      const foundUser = await User.find({
-        _id: context.user._id,
-      });
-      return foundUser;
-    },
+    me: async (parent, { id }, context) => {
+      const me = await User.findOne({ _id: new Types.ObjectId(id) }).populate('savedBooks');
+      return me;
+    }
   },
   Mutation: {
-    addUser: async (parent, args, context) => {
-      const user = await User.create(...args);
-      console.log(args)
+    addUser: async (parent,  args, context) => {
+      const user = await User.create({
+        username: args.username,
+        email: args.email,
+        password: args.password,
+      });
       const token = signToken(user);
       return { token, user };
     },
-    login: async(parents, args, context) => {
+    login: async(parents, { email, password }, context) => {
       const user = await User.findOne({ email });
       if (!user) {
         throw new AuthenticationError('No user found with this email address');
@@ -31,25 +33,24 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    saveBook: async (parent, args, context) => {
+    saveBook: async (parent, { bookData }, context) => {
       if (context.user) {
         try {
-          const newBook = new Book(bookData);
           const updatedUser = await User.findOneAndUpdate(
             { _id: context.user._id },
-            { $addToSet: { savedBooks: newBook } },
+            { $addToSet: { savedBooks: bookData } },
             { new: true }
           );
           return updatedUser;
         } catch (err) {
           console.log(err);
-          throw new Error('Could not save book');
+          throw new Error('Error saving book');
         }
       } else {
         throw new AuthenticationError('You need to be logged in!');
       }
     },
-    removeBook: async (parent, args, context) => {
+    removeBook: async (parent, { id }, context) => {
       if (context.user) {
         try{ 
           const updatedUser = await User.findOneAndUpdate(

58825608f7c256df0e7bddb711a26601adaecbea
Avery Myers <averyjmyers@hotmail.com>
2023-10-16 15:30:07 -0700

    working auth

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -11,9 +11,7 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => {
-    return authMiddleware(req);
-  },
+  context: authMiddleware
 });
 
 
--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -5,30 +5,33 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req) {
-    console.log('we hit authMiddleware')
-    let token = req.query.token || req.body.token || req.headers.authorization;
-    console.log(token)
-  
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
-  
-    if (!token) {
-      throw new Error('You have no token!');
-    }
-  
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      return { user: data };
-    } catch {
-      throw new Error('Invalid token');
-    }
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+  authMiddleware: function ({ req }) {
+      // console.log('we hit authMiddleware')
+      let token = req.query.token || req.body.token || req.headers.authorization;
+    
+      if (req.headers.authorization) {
+        token = token.split(' ').pop().trim();
+      }
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+      // if (!token) {
+      //   throw new Error('You have no token!');
+      // }
+    
+      try {
+        const { data } = jwt.verify(token, secret, { maxAge: expiration });
+        req.user = data;
+        } catch {
+        // throw new Error('Invalid token');
+      
+      }
+      return req;
+  },
+
+  signToken: function ({ username, email, _id }) {
+      const payload = { username, email, _id };
+  
+      return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+      
+      // return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };

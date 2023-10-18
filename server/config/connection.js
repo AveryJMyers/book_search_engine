937c758013fa4a87fbd2d2f83587f70a2dacdc27
Avery Myers <averyjmyers@hotmail.com>
2023-10-17 21:30:13 -0700

    updated db

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,10 +5,4 @@ mongoose.connect(process.env.MONGODB_URI, {
     useUnifiedTopology: true,
   });
 
-  const db = mongoose.connection;
-
-  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-  
-  db.once('open', () => {
-    console.log('Connected to MongoDB');
-  });
+export default mongoose.connection;
